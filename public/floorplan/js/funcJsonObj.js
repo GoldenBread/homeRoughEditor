781fa6306a3f5c53dd6366d71dab8f6d6f1e5aec
Thierry Levisse <thierry.levisse@ideine.fr>
2020-11-18 15:52:58 +0100

    Added diamond flyingwing rectangle import/export

--- public/floorplan/js/funcJsonObj.js
+++ public/floorplan/js/funcJsonObj.js
@@ -21,7 +21,76 @@ function createWall(pointA, pointB, roomShape) {
 
 //
 //Import
-function createDiamondShapeRoom() {
+function importRooms() {
+    var jsonStr = document.getElementById('saveCurrentPlan').value;
+    var sample = JSON.parse(jsonStr);
+
+    sample.rooms.forEach(room => {
+        switch (room.shape) {
+            case "rectangle":
+                createRectangleShapeRoom(room);
+                break;
+            case "diamond_topleft":
+            case "diamond_topright":
+            case "diamond_bottomleft":
+            case "diamond_bottomright":
+                createDiamondShapeRoom(room)
+                break;
+            case "flyingwing_topleft":
+            case "flyingwing_topright":
+            case "flyingwing_bottomleft":
+            case "flyingwing_bottomright":
+                createFlyingWingShapeRoom(room);
+                break;
+        }
+    })
+}
+
+function createRectangleShapeRoom(room) {
+    var points = [
+        {
+            x: 0,
+            y: 0
+        },
+        {
+            x: room.dimensions.width,
+            y: 0
+        },
+        {
+            x: room.dimensions.width,
+            y: room.dimensions.height
+        },
+        {
+            x: 0,
+            y: room.dimensions.height
+        }
+    ];
+
+    var sides = new Map([
+        ["top", [points[0], points[1]]],
+        ["left", [points[1], points[2]]],
+        ["bottom", [points[2], points[3]]],
+        ["right", [points[3], points[0]]]
+    ]);
+
+    translation(points, room.center);
+
+    points.forEach(point => point = convertToCoordinates(point));
+
+    createWall(points[0], points[1], room.shape);
+    createWall(points[1], points[2], room.shape);
+    createWall(points[2], points[3], room.shape);
+    createWall(points[3], points[0], room.shape);
+
+    scanObjects(room.objects, sides);
+}
+
+var objectCorrespImport = new Map([
+    ["doors", "aperture"],
+    ["windows", "fix"]
+]);
+
+function createDiamondShapeRoom(room) {
     var angleShape = new Map([
         ["diamond_topleft", 0],
         ["diamond_topright", 90],
@@ -29,16 +98,8 @@ function createDiamondShapeRoom() {
         ["diamond_bottomright", 270],
     ]);
 
-    var objectCorresp = new Map([
-        ["doors", "aperture"],
-        ["windows", "fix"]
-    ]);
-
-    var jsonStr = document.getElementById('saveCurrentPlan').value;
-    var sample = JSON.parse(jsonStr);
-
     //define points coordinates
-    var tableCommonPoint = Math.sqrt(Math.pow(sample.rooms[0].dimensions.table, 2) / 2);
+    var tableCommonPoint = Math.sqrt(Math.pow(room.dimensions.table, 2) / 2);
 
     var points = [
         {
@@ -50,16 +111,16 @@ function createDiamondShapeRoom() {
             y: 0
         },
         {
-            x: tableCommonPoint + sample.rooms[0].dimensions.right_crown,
+            x: tableCommonPoint + room.dimensions.right_crown,
             y: 0
         },
         {
-            x: tableCommonPoint + sample.rooms[0].dimensions.right_crown,
-            y: tableCommonPoint + sample.rooms[0].dimensions.left_crown
+            x: tableCommonPoint + room.dimensions.right_crown,
+            y: tableCommonPoint + room.dimensions.left_crown
         },
         {
             x: 0,
-            y: tableCommonPoint + sample.rooms[0].dimensions.left_crown
+            y: tableCommonPoint + room.dimensions.left_crown
         }
     ]
 
@@ -71,36 +132,98 @@ function createDiamondShapeRoom() {
         ["left_pavilion", [points[3], points[4]]]
     ]);
 
-    rotation(points, angleShape.get(sample.rooms[0].shape));
+    rotation(points, angleShape.get(room.shape));
 
     //move points to correct location
-    translation(points, sample.rooms[0].center);
+    translation(points, room.center);
 
-    points[0] = convertToCoordinates(points[0]);
-    points[1] = convertToCoordinates(points[1]);
-    points[2] = convertToCoordinates(points[2]);
-    points[3] = convertToCoordinates(points[3]);
-    points[4] = convertToCoordinates(points[4]);
+    //convert meter to svg coordinates
+    points.forEach(point => point = convertToCoordinates(point));
 
     //draw table
-    createWall(points[0], points[1], sample.rooms[0].shape);
+    createWall(points[0], points[1], room.shape);
 
     //draw right crown
-    createWall(points[1], points[2], sample.rooms[0].shape);
+    createWall(points[1], points[2], room.shape);
 
     //draw right pavilion
-    createWall(points[2], points[3], sample.rooms[0].shape);
+    createWall(points[2], points[3], room.shape);
 
     //draw left pavilion
-    createWall(points[3], points[4], sample.rooms[0].shape);
+    createWall(points[3], points[4], room.shape);
 
     //draw left crown
-    createWall(points[4], points[0], sample.rooms[0].shape);
+    createWall(points[4], points[0], room.shape);
 
     //draw doors, windows
+    scanObjects(room.objects, sides);
+}
+
+function createFlyingWingShapeRoom(room) {
+    var angleShape = new Map([
+        ["flyingwing_topleft", 0],
+        ["flyingwing_topright", 90],
+        ["flyingwing_bottomleft", 180],
+        ["flyingwing_bottomright", 270],
+    ]);
+
+    //define points coordinates
+    var points = [
+        {
+            x: 0,
+            y: 0
+        },
+        {
+            x: room.dimensions.left_wingtip + room.dimensions.right_trailing_edge,
+            y: 0
+        },
+        {
+            x: room.dimensions.left_wingtip + room.dimensions.right_trailing_edge,
+            y: room.dimensions.right_wingtip
+        },
+        {
+            x: room.dimensions.left_wingtip,
+            y: room.dimensions.right_wingtip
+        },
+        {
+            x: room.dimensions.left_wingtip,
+            y: room.dimensions.right_wingtip + room.dimensions.left_trailing_edge
+        },
+        {
+            x: 0,
+            y: room.dimensions.right_wingtip + room.dimensions.left_trailing_edge
+        }
+    ];
+
+    var sides = new Map([
+        ["right_leading_edge", [points[0], points[1]]],
+        ["right_wingtip", [points[1], points[2]]],
+        ["right_trailing_edge", [points[2], points[3]]],
+        ["left_trailing_edge", [points[3], points[4]]],
+        ["left_wingtip", [points[4], points[5]]],
+        ["left_leading_edge", [points[5], points[0]]]
+    ]);
+
+    rotation(points, angleShape.get(room.shape));
+
+    translation(points, room.center);
+
+    points.forEach(point => point = convertToCoordinates(point));
+
+    createWall(points[0], points[1], room.shape);
+    createWall(points[1], points[2], room.shape);
+    createWall(points[2], points[3], room.shape);
+    createWall(points[3], points[4], room.shape);
+    createWall(points[4], points[5], room.shape);
+    createWall(points[5], points[0], room.shape);
+
+    scanObjects(room.objects, sides);
+}
+
+function scanObjects(objects, sides) {
     ["doors", "windows"].forEach(type => {
-        if (sample.rooms[0].objects[type] !== undefined) {
-            sample.rooms[0].objects[type].forEach(obj => {
+        if (objects[type] !== undefined) {
+            objects[type].forEach(obj => {
                 var location_side = obj.location_side;
                 var wallFound = WALLS.find(x => (sides.get(location_side)[0] == x.start || sides.get(location_side)[0] == x.end) && (sides.get(location_side)[1] == x.start || sides.get(location_side)[1] == x.end));//TLE TODO handle line with 2 same points
             
@@ -108,7 +231,7 @@ function createDiamondShapeRoom() {
                 var xCenterObj = (1 - ratio) * sides.get(location_side)[0].x + ratio * sides.get(location_side)[1].x;
                 var yCenterObj = (1 - ratio) * sides.get(location_side)[0].y + ratio * sides.get(location_side)[1].y;
                 
-                createObjects({wall: wallFound, x: xCenterObj, y: yCenterObj}, objectCorresp.get(type), obj.size * meter);
+                createObjects({wall: wallFound, x: xCenterObj, y: yCenterObj}, objectCorrespImport.get(type), obj.size * meter);
             });
         }
     });
@@ -216,13 +339,73 @@ function rollArray(array, count) {
     return [...array.slice(count, array.length), ...array.slice(0, count)];
 }
 
-function exportDiamondShapeRoom() {
-    var originalWall = ROOM[0];
-    var door = OBJDATA[0];
+function exportRooms() {
+    ROOM.forEach(room => {
+        switch (room.roomShape) {
+            case "rectangle":
+                exportRectangleShapeRoom(room);
+                break;
+            case "diamond_topleft":
+            case "diamond_topright":
+            case "diamond_bottomleft":
+            case "diamond_bottomright":
+                exportDiamondShapeRoom(room)
+                break;
+            case "flyingwing_topleft":
+            case "flyingwing_topright":
+            case "flyingwing_bottomleft":
+            case "flyingwing_bottomright":
+                exportFlyingWingShapeRoom(room);
+                break;
+        }
+    });
+}
 
-    var points = _.cloneDeep(originalWall.coords.slice(0, originalWall.coords.length - 1));
+function exportRectangleShapeRoom(room) {
+    var points = _.cloneDeep(room.coords.slice(0, room.coords.length - 1));
 
-    switch (originalWall.roomShape) {
+    points = rollArray(points, 2);
+
+    var sides = new Map([
+        ["top", [points[1], points[0]]],
+        ["left", [points[2], points[1]]],
+        ["bottom", [points[3], points[2]]],
+        ["right", [points[0], points[3]]]
+    ]);
+
+    objects = exportObjects(OBJDATA, sides);
+
+    points.forEach(point => point = convertToMeters(point));
+
+    var mp = meanPoint(points);
+
+    translation(points, mp, true);
+
+    var widthDistance, heightDistance;
+    widthDistance = distanceBetweenPoints(sides.get("top")[0], sides.get("top")[1]);
+    heightDistance = distanceBetweenPoints(sides.get("left")[0], sides.get("left")[1]);
+
+    var sample = {
+        rooms: [
+            {
+                shape: room.roomShape,
+                dimensions: {
+                    width: widthDistance,
+                    height: heightDistance
+                },
+                center: mp,
+                objects: objects
+            }
+        ]
+    };
+
+    document.getElementById('saveCurrentPlan').value = JSON.stringify(sample, null, 4);
+}
+
+function exportDiamondShapeRoom(room) {
+    var points = _.cloneDeep(room.coords.slice(0, room.coords.length - 1));
+
+    switch (room.roomShape) {
         case "diamond_topleft":
             points = rollArray(points, 3);
             break;
@@ -242,28 +425,7 @@ function exportDiamondShapeRoom() {
         ["right_pavilion", [points[4], points[3]]]
     ]);
 
-    var objectCorresp = new Map([
-        ["aperture", "doors"],
-        ["fix", "windows"]
-    ]);
-
-    var objects = {};
-
-    OBJDATA.forEach(object => {
-        Array.from(sides).forEach(side => {
-            if (isLineBOnLineA(side[1], object.limit)) {
-                var exportType = objectCorresp.get(object.type);
-                if (!objects.hasOwnProperty(exportType)) {
-                    objects[exportType] = [];
-                }
-                objects[exportType].push({
-                    location_side: side[0],
-                    location_position_from_edge: distanceBetweenPoints(side[1][0], { x: object.x, y: object.y }) / meter,
-                    size: object.size / meter
-                });
-            }
-        });
-    });
+    objects = exportObjects(OBJDATA, sides);
 
     points.forEach(point => point = convertToMeters(point));
 
@@ -279,7 +441,7 @@ function exportDiamondShapeRoom() {
     var sample = {
         rooms: [
             {
-                shape: originalWall.roomShape,
+                shape: room.roomShape,
                 dimensions: {
                     left_crown: left_crownDistance,
                     table: tableDistance,
@@ -291,5 +453,89 @@ function exportDiamondShapeRoom() {
         ]
     };
 
-    document.getElementById('saveCurrentPlan').value = JSON.stringify(sample, null, 4);;
+    document.getElementById('saveCurrentPlan').value = JSON.stringify(sample, null, 4);
 }
+
+function exportFlyingWingShapeRoom(room) {
+    var points = _.cloneDeep(room.coords.slice(0, room.coords.length - 1));
+
+    switch (room.roomShape) {
+        case "flyingwing_topleft":
+            points = rollArray(points, 4);
+            break;
+        case "flyingwing_topright":
+            points = rollArray(points, 3);
+            break;
+        case "flyingwing_bottomleft":
+            points = rollArray(points, 2);
+            break;
+    }
+
+    var sides = new Map([
+        ["right_leading_edge", [points[1], points[0]]],
+        ["left_leading_edge", [points[2], points[1]]],
+        ["left_wingtip", [points[3], points[2]]],
+        ["left_trailing_edge", [points[4], points[3]]],
+        ["right_trailing_edge", [points[5], points[4]]],
+        ["right_wingtip", [points[0], points[5]]]
+    ]);
+
+    objects = exportObjects(OBJDATA, sides);
+
+    points.forEach(point => point = convertToMeters(point));
+
+    var mp = meanPoint(points);
+
+    translation(points, mp, true);
+
+    var right_wingtipDistance, right_trailing_edgeDistance, left_trailing_edgeDistance, left_wingtipDistance;
+    right_wingtipDistance = distanceBetweenPoints(sides.get("right_wingtip")[0], sides.get("right_wingtip")[1]);
+    right_trailing_edgeDistance = distanceBetweenPoints(sides.get("right_trailing_edge")[0], sides.get("right_trailing_edge")[1]);
+    left_trailing_edgeDistance = distanceBetweenPoints(sides.get("left_trailing_edge")[0], sides.get("left_trailing_edge")[1]);
+    left_wingtipDistance = distanceBetweenPoints(sides.get("left_wingtip")[0], sides.get("left_wingtip")[1]);
+
+    var sample = {
+        rooms: [
+            {
+                shape: room.roomShape,
+                dimensions: {
+                    right_wingtip: right_wingtipDistance,
+                    right_trailing_edge: right_trailing_edgeDistance,
+                    left_trailing_edge: left_trailing_edgeDistance,
+                    left_wingtip: left_wingtipDistance
+                },
+                center: mp,
+                objects: objects
+            }
+        ]
+    };
+
+    document.getElementById('saveCurrentPlan').value = JSON.stringify(sample, null, 4);
+}
+
+var objectCorrespExport = new Map([
+    ["aperture", "doors"],
+    ["fix", "windows"]
+]);
+
+function exportObjects(objects, sides) {
+    var exportedObjects = {};
+
+    objects.forEach(object => {
+        Array.from(sides).forEach(side => {
+            if (isLineBOnLineA(side[1], object.limit)) {
+                var exportType = objectCorrespExport.get(object.type);
+                if (!exportedObjects.hasOwnProperty(exportType)) {
+                    exportedObjects[exportType] = [];
+                }
+                exportedObjects[exportType].push({
+                    location_side: side[0],
+                    location_position_from_edge: distanceBetweenPoints(side[1][0], { x: object.x, y: object.y }) / meter,
+                    size: object.size / meter
+                });
+            }
+        });
+    });
+    return exportedObjects;
+} 
+
